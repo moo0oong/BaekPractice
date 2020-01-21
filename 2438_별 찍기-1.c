46d11c4d0a03d896a23ec6be8e87f8bfb2bc8b6d
moo0oong <59860229+moo0oong@users.noreply.github.com>
2020-01-21 16:09:21 +0900

    Create 2438_별 찍기-1.c

---
+++ 2438_별 찍기-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+/*첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제.*/
+
+	int count;
+	
+	scanf("%d", &count);
+	
+	for (int i = 0; i < count; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+
+	return 0;
+}
