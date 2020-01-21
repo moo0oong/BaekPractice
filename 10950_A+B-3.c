72d7aa7113a038d4a0712a3a7b7bb3ae6dc33883
moo0oong <59860229+moo0oong@users.noreply.github.com>
2020-01-21 16:27:46 +0900

    Update 10950_A+B-3.c
    단순한 버그라고 생각하고 넘어가면 좋을 것 같다.

--- 10950_A+B-3.c
+++ 10950_A+B-3.c
@@ -4,7 +4,7 @@ int main() {
     scanf("%d", &t); 
     int a, b;
     for (int i = 0; i < t; i++) { 
-        scanf("%d %d", &a, &b); 
+        scanf("%d %d", &a, &b);  //scanf에서 개행이 있으면 한번 더 입력하게된다. 왜 그런지 고민해보자.
         printf("%d\n", a + b); 
     }
     return 0;
