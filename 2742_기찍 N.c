45485e25aa81c65b61e6b8fc2232a54844602c13
moo0oong <59860229+moo0oong@users.noreply.github.com>
2020-01-21 16:12:59 +0900

    Create 2742_기찍 N.c

---
+++ 2742_기찍 N.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+/*자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.*/
+
+	int count;
+	
+	scanf("%d", &count);
+	
+	for (int i = 1; i <= count; i++) {
+		printf("%d\n", count-i+1);
+	}
+	
+	return 0;
+}
